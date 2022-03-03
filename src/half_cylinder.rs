541263f7ab389c7d72563301fa76bda2eaf88bd0
Robert Swain <robert.swain@gmail.com>
2022-03-03 12:04:41 +0100

    Add half-cylinder shape and Mesh implementation

---
+++ src/half_cylinder.rs
@@ -0,0 +1,79 @@
+use bevy::{
+    math::{Quat, Vec3},
+    prelude::Mesh,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+};
+
+pub struct HalfCylinder {
+    start: Vec3,
+    end: Vec3,
+    radius: f32,
+    subdivisions: usize,
+}
+
+impl Default for HalfCylinder {
+    fn default() -> Self {
+        Self {
+            start: -0.5 * Vec3::Z,
+            end: 0.5 * Vec3::Z,
+            radius: 0.5,
+            subdivisions: 10,
+        }
+    }
+}
+
+impl From<HalfCylinder> for Mesh {
+    fn from(shape: HalfCylinder) -> Self {
+        let HalfCylinder {
+            start,
+            end,
+            radius,
+            subdivisions,
+        } = shape;
+        let vertex_count = (subdivisions + 1) * 2;
+
+        let mut positions = Vec::with_capacity(vertex_count);
+        let mut normals = Vec::with_capacity(vertex_count);
+        let mut uvs = Vec::with_capacity(vertex_count);
+
+        let up = Vec3::Y;
+        let forward = (end - start).normalize_or_zero();
+        let right = up.cross(-forward).normalize_or_zero() * radius;
+        for i in 0..=subdivisions {
+            // start point
+            let offset = Quat::from_axis_angle(
+                forward,
+                std::f32::consts::PI * i as f32 / subdivisions as f32,
+            ) * right;
+            let normal = (-offset.normalize_or_zero()).to_array();
+            positions.push((start + offset).to_array());
+            normals.push(normal);
+            uvs.push([0.0, 0.0]);
+            // end point
+            positions.push((end + offset).to_array());
+            normals.push(normal);
+            uvs.push([0.0, 0.0]);
+        }
+
+        let mut indices = Vec::with_capacity(subdivisions * 2);
+        for i in 0..subdivisions as u32 {
+            let offset = i as u32 * 2;
+            indices.extend_from_slice(&[
+                offset + 2,
+                offset,
+                offset + 1,
+                offset + 1,
+                offset + 3,
+                offset + 2,
+            ]);
+        }
+        let indices = Indices::U32(indices);
+
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh.set_indices(Some(indices));
+        mesh
+    }
+}
